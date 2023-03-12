794bfa6eb7e8e82491ce3732e2258e1affd8d7ca
tasbiha11 <tasbihahussain@yahoo.com>
2023-03-12 22:13:08 +0600

    side-menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,29 +4,58 @@ import { FaMicrophone } from 'react-icons/fa';
 import { RxAvatar } from 'react-icons/rx';
 import { IoMdNotificationsOutline } from 'react-icons/io';
 import { BsSearch } from 'react-icons/bs';
+import { GrHomeRounded } from 'react-icons/gr';
+import { SiYoutubemusic } from 'react-icons/si';
+import { MdOutlineSubscriptions, MdOutlineWatchLater } from 'react-icons/md';
 import logo from '../../assets/logo.png';
 import './Header.css';
 
 const Header = () => {
+
+
     return (
-        <div className='top'>
-            <div className='top-left'>
-                <AiOutlineMenu />
-                <img className='yt-logo' src={logo} alt="logo" />
-            </div>
-            <div className='top-search'>
-                <div className='top-middle'>
-                    <input className='search-box' type="text" placeholder='Search' />
-                    <BsSearch className='search-icon' />
+        <>
+            <div className='top'>
+                <div className='top-left'>
+                    <AiOutlineMenu />
+                    <img className='yt-logo' src={logo} alt="logo" />
                 </div>
-                <FaMicrophone className='icons' />
+                <div className='top-search'>
+                    <div className='top-middle'>
+                        <input className='search-box' type="text" placeholder='Search' />
+                        <BsSearch className='search-icon' />
+                    </div>
+                    <FaMicrophone className='icons' />
+                </div>
+                <div className='top-right'>
+                    <AiOutlineVideoCameraAdd className='icons' />
+                    <IoMdNotificationsOutline className='icons' />
+                    <RxAvatar className='icons' />
+                </div>
             </div>
-            <div className='top-right'>
-                <AiOutlineVideoCameraAdd className='icons' />
-                <IoMdNotificationsOutline className='icons' />
-                <RxAvatar className='icons' />
+
+            <div className="side-menu">
+                <div className="collapse-menu">
+                    <div className="mini-menu">
+                        <GrHomeRounded />
+                        <p>Home</p>
+                    </div>
+                    <div className="mini-menu">
+                        <SiYoutubemusic />
+                        <p>Shorts</p>
+                    </div>
+                    <div className="mini-menu">
+                        <MdOutlineSubscriptions />
+                        <p>Subscription</p>
+                    </div>
+                    <div className="mini-menu">
+                        <MdOutlineWatchLater />
+                        <p>Watch Later</p>
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
+
     )
 }
 
